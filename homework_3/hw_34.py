4a7095b05fbcd7c73e084999219a1c3466929062
kolosova-anna <golikovaanna7365@mail.ru>
2022-12-25 23:44:54 +0200

    hw_34 - done

--- homework_3/hw_34.py
+++ homework_3/hw_34.py
@@ -13,17 +13,13 @@
 
 from random import random
 number = int (input('Введите количество элементов: '))
-# def float_numbers (num):
-#     new_list = []
-#     for i in range (num):
-#         new_list.append (round ((random() * 10), 2))
-#     print(new_list)
 
 def find_difference (num):
     new_list = []
     for i in range (num):
         new_list.append (round ((random() * 10), 2))
     print(new_list)
+
     num_min = new_list[0]
     num_max = new_list[0]
     i = 1
@@ -31,19 +27,20 @@ def find_difference (num):
         if new_list[i] < num_min:
             num_min = new_list[i]
         i += 1
-    print (num_min)
+    print ('min =', (num_min))
+
     i = 1
     while i < len(new_list):
         if new_list[i] > num_max:
             num_max = new_list[i]
         i += 1
-    print (num_max)
-    num_1 = round (num_min - int(num_min), 2)
-    print (num_1)
-    num_2 = round (num_max - int(num_max), 2)
-    print (num_2)
-    resalt = round ((num_2 - num_1), 2)
-    print (resalt)
+    print ('max =', (num_max))
 
-# float_nums = float_numbers (number)
+    num_1 = round (num_min - int(num_min), 2)
+    num_2 = round (num_max - int(num_max), 2)
+    resalt = round ((num_2 - num_1), 2)
+
+    print ('differense =', (resalt))
+
+
 find_difference (number)
