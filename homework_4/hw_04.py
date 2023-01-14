1d2fbafcf56218d0c6df0e9e876e8152de0ef457
kolosova-anna <golikovaanna7365@mail.ru>
2023-01-14 23:22:05 +0200

    Add hw_04

--- homework_4/hw_04.py
+++ homework_4/hw_04.py
@@ -9,3 +9,24 @@
 # 4*x^5 + 1*x^4 - 3*x^3 - 3 = 0
 # 4*x^2 - 4 = 0
 
+from random import choice, sample
+
+def polynominal(num: int):
+    if num < 1:
+        return 0
+    
+    poly = ""
+    num_list = range (0, 11)
+
+    with open("poly_2.txt", "a", encoding = "utf-8") as my_f:
+        for i in range (num, 1, -1):
+            value = choice(num_list)
+            if value:
+                poly += f"{value}*x^{i} {choice('+-')}"
+
+        numbers = sample(range(1, 11), k=2)
+        my_f.write(f"{poly}{choice(numbers)}*x {choice('+-')} {choice(numbers)} = 0\n")
+
+
+for _ in range(3):
+    polynominal(int(input()))
